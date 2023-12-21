039713df5c8723d953033cf1cd4c0ce764cd4efc
Minettii <rafael_minetti@hotmail.com>
2023-12-20 21:17:51 -0300

    Exercício 11

---
+++ exercicio11/index.js
@@ -0,0 +1,38 @@
+let escalacao = document.getElementById("escalacao");
+
+function escalarJogador() {
+  if (
+    confirm(
+      "Deseja escalar o jogador " +
+        document.getElementById("addNome").value +
+        " como " +
+        document.getElementById("addPosicao").value +
+        ", camisa " +
+        document.getElementById("addCamisa").value +
+        "?"
+    )
+  ) {
+    let newLi = document.createElement("li");
+    newLi.id = document.getElementById("addCamisa").value;
+    newLi.innerHTML =
+      document.getElementById("addNome").value +
+      ", " +
+      document.getElementById("addPosicao").value +
+      ", camisa " +
+      document.getElementById("addCamisa").value;
+    escalacao.appendChild(newLi);
+    document.getElementById("addPosicao").value = "";
+    document.getElementById("addNome").value = "";
+    document.getElementById("addCamisa").value = "";
+  }
+}
+
+function removerJogador() {
+  let jogador = document.getElementById(
+    document.getElementById("removeCamisa").value
+  );
+  if (confirm("Deseja remover o jogador " + jogador.innerHTML + "?")) {
+    escalacao.removeChild(jogador);
+    document.getElementById("removeCamisa").value = "";
+  }
+}
