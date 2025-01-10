798976c5fb48a9e1c5b87ddad1602dd8b59e8c19
YOUR_NAME <rafael_minetti@hotmail.com>
2025-01-10 09:59:16 -0300

    feat(exercise17): add project
    Uses babel to compile js
    
    Uses webpack to bundle js and css

---
+++ exercise17/src/index.js
@@ -0,0 +1,5 @@
+const dayjs = require("dayjs");
+import "./styles/index.css";
+
+const todayDate = dayjs().format("DD/MM/YYYY");
+alert(todayDate);
---
+++ exercise17/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+	entry: {
+		index: "./src/index.js",
+	},
+	mode: "production",
+	output: {
+		filename: "[name].min.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.js$/,
+				use: ["babel-loader"],
+			},
+		],
+	},
+};
